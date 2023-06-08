1a91b156fee1be49c9f9765e598a32742f2303f3
Walter Hulsebos <walterhulsebos@outlook.com>
2023-06-08 14:02:16 +0200

    Encounter Spawn Probabilities

--- __DeathRunner_UnityProj__/Assets/03_Scripts/03_Enemies/Encounter.cs
+++ __DeathRunner_UnityProj__/Assets/03_Scripts/03_Enemies/Encounter.cs
@@ -1,14 +1,25 @@
 using DeathRunner.Utils;
+using Sirenix.OdinInspector;
 using UnityEngine;
+
+using F32  = System.Single;
+using U16  = System.UInt16;
 
 namespace DeathRunner.Enemies
 {
     /// <summary> An enemy encounter without waves. </summary>
     public sealed class Encounter : MonoBehaviour
     {
-        [SerializeField] private Transform[] spawnPoints;
+        [Probability(dataMember: nameof(enemyPrefabs))]
+        [SerializeField] private F32[] spawnProbability;
         
-        [Probability("")]
-        [SerializeField] private Enemy[]
+        [AssetsOnly]
+        [SerializeField] private Enemy[] enemyPrefabs;
+        
+        [BoxGroup("Spawn Settings", showLabel: false)]
+        [SerializeField] private U16 totalAmountToSpawn = 4;
+        
+        [Space]
+        [SerializeField] private Transform[] spawnPoints;
     }
 }
