adf08c07484ff54681be26feeaf5d7bb8c6ddeaa
Cowboy <peteran20@outlook.com>
2023-06-13 03:38:16 +0200

    Build1

--- __DeathRunner_UnityProj__/Assets/AOEAttack.cs
+++ __DeathRunner_UnityProj__/Assets/AOEAttack.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 namespace DeathRunner
 {
     public class AOEAttack : MonoBehaviour
     {
-       [SerializeField] private GameObject hitbox;
-        
+        [SerializeField] private GameObject hitbox;
+
+        [SerializeField] private GameObject healthDrop;
         // Start is called before the first frame update
         void Start()
         {
@@ -21,6 +23,7 @@ namespace DeathRunner
             hitbox.SetActive(true);
             yield return new WaitForSeconds(0.5f);
             hitbox.SetActive(false);
+            Instantiate(healthDrop, transform.position, quaternion.identity);
             yield return new WaitForSeconds(1.5f);
             gameObject.SetActive(false);
         }
